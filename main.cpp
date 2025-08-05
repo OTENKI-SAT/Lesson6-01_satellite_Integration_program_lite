8bc5b8b4b66b2e8e40eb693f5579b49585934c59
sonenagisa <93703736+sonenagisa@users.noreply.github.com>
2025-08-05 09:34:57 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,70 +1,101 @@
 #include "mbed.h"
-#include "LITE_EPS.h"
-#include "LITE_SENSOR.h"
-#include "LITE_CDH.h"
-#include "LITE_COM.h"
-LITE_EPS eps(PA_0,PA_4);
-LITE_SENSOR sensor(PA_7,PB_7,PB_6);
-RawSerial pc(USBTX,USBRX,9600);
-LITE_CDH cdh(PB_5, PB_4, PB_3, PA_8, "sd", PA_3);
-LITE_COM com(PA_9,PA_10,9600);
+#include "HEPTA_EPS.h"
+#include "HEPTA_CDH.h"
+#include "HEPTA_SENSOR.h"
+#include "HEPTA_COM.h"
+HEPTA_CDH cdh(PB_5, PB_4, PB_3, PA_8, "sd");
+HEPTA_EPS eps(PA_0,PA_4);
+HEPTA_SENSOR sensor(PA_7,PB_7,PB_6,0xD0);
+HEPTA_COM com(PA_9,PA_10,9600);
+DigitalOut condition(PB_1);
+RawSerial sat(USBTX,USBRX, 38400);
 Timer sattime;
 int main()
-{   
+{
+    sat.printf("From Sat : Nominal Operation\r\n");
+    com.printf("From Sat : Nominal Operation\r\n");
+    int flag = 0; //condition flag
+    float batvol, temp; //voltage, temperature 
+    int rcmd=0,cmdflag=0;  //command variable
     sattime.start();
-    mkdir("/sd/mydir", 0777);
-    FILE *fp = fopen("/sd/mydir/test.txt","w");
-    if(fp == NULL) {
-        error("Could not open file for write\r\n");
-    }
-    char str[100];
-    float ax,ay,az,gx,gy,gz,mx,my,mz;
-    float temp;
-    float batvol;
-    eps.turn_on_regulator(); //Turn on 3.3V converter
-    sensor.set_up();
-    cdh.turn_on_analogSW();//turn on transceiver
-    int rcmd=0,cmdflag=0;
-    while(1){
-        //各センサーの値を取得
-        eps.vol(&batvol);
-        sensor.temp_sense(&temp);
-        sensor.sen_acc(&ax,&ay,&az);
-        sensor.sen_gyro(&gx,&gy,&gz);
-        sensor.sen_mag(&mx,&my,&mz);
-        //SDにデータを保存
-        fprintf(fp,"%f,%f,%f,",sattime.read(),batvol,temp);
-        fprintf(fp,"%f,%f,%f,",ax,ay,az);
-        fprintf(fp,"%f,%f,%f,",gx,gy,gz);
-        fprintf(fp,"%f,%f,%f\r\n",mx,my,mz);
-        //地上局にデータを送信
-        com.printf("%f%f%f%f%f%f%f%f%f%f%f%f",sattime.read(),batvol,temp,ax,ay,az,gx,gy,gz,mx,my,mz);
-        //1秒待機（1秒間隔でデータを取得する）
-        wait(1);
-        
-        //地上局からのコマンドを受信
+    eps.turn_on_regulator();//turn on 3.3V conveter
+    sensor.setup();  
+    for(int i=0;i<50;i++){
         com.xbee_receive(&rcmd,&cmdflag);
         
-        if (cmdflag == 1) {
-            if (rcmd == 'a') {//コマンドが"a"かを識別
-                //"a"だったら下記を実行
-                for(int i = 0; i < 100; i++){
-                    //各センサーの値を取得
+        //satellite condition led
+        condition = !condition;
+        
+        //senssing HK data
+        eps.vol(&batvol);
+        sensor.temp_sense(&temp);
+        
+        //Transmitting HK data to Ground Station(GS)
+        com.printf("HEPTASAT::Condition = %d, Time = %f [s], batvol = %2f [V], temp = %2f [deg C]\r\n",flag,sattime.read(),batvol,temp);
+        wait_ms(1000);
+        
+        //Power Saving Mode 
+        if((batvol <= 3.5)  | (temp > 35.0)){
+            eps.shut_down_regulator();
+            com.printf("Power saving mode ON\r\n"); 
+            flag = 1;
+        } else if((flag == 1) & (batvol > 3.7) & (temp <= 25.0)) {
+            eps.turn_on_regulator();
+            com.printf("Power saving mode OFF\r\n");
+            flag = 0;
+        }
+        
+        if(cmdflag == 1){
+            if(rcmd == 'a'){
+                sat.printf("rcmd=%c,cmdflag=%d\r\n",rcmd,cmdflag);
+                eps.vol(&batvol);
+                sensor.temp_sense(&temp);
+                com.printf("Hepta-Sat Lite Uplink Ok\r\n");
+                for(int j=0;j<5;j++){
+                    com.printf("Time = %.f [s], voltage = [V], Temperature=%f[degC]\r\n",sattime.read(),batvol,temp);
+                    condition = 1;
+                    wait_ms(1000);
+                }
+            }else if(rcmd == 'b') {
+                sat.printf("rcmd=%c,cmdflag=%d\r\n",rcmd,cmdflag);
+                com.printf("Hepta-Sat Lite Uplink Ok\r\n");
+                char str[100];
+                mkdir("/sd/mydir", 0777);
+                FILE *fp = fopen("/sd/mydir/satdata.txt","w");
+                if(fp == NULL) {
+                    error("Could not open file for write\r\n");
+                }
+                for(int i = 0; i < 10; i++) {
                     eps.vol(&batvol);
-                    sensor.temp_sense(&temp);
+                    fprintf(fp,"%f\r\n",batvol);
+                    condition = 1;
+                    wait_ms(1000);
+                }
+                fclose(fp);
+                fp = fopen("/sd/mydir/satdata.txt","r");
+                for(int i = 0; i < 10; i++) {
+                    fgets(str,100,fp);
+                    com.puts(str);
+                }
+                fclose(fp);
+            }else if(rcmd == 'c'){
+                sat.printf("rcmd=%c,cmdflag=%d\r\n",rcmd,cmdflag);
+                com.printf("Hepta-Sat Lite Uplink Ok\r\n");
+                float ax,ay,az,gx,gy,gz,mx,my,mz;
+                for(int i = 0; i < 10; i++) {
                     sensor.sen_acc(&ax,&ay,&az);
                     sensor.sen_gyro(&gx,&gy,&gz);
                     sensor.sen_mag(&mx,&my,&mz);
-                    //SDにデータを保存
-                    fprintf(fp,"%f,%f,%f",sattime.read(),batvol,temp);
-                    fprintf(fp,"%f,%f,%f",ax,ay,az);
-                    fprintf(fp,"%f,%f,%f",gx,gy,gz);
-                    fprintf(fp,"%f,%f,%f\r\n",mx,my,mz);
-                    //地上局にデータを送信
-                    com.printf("%f%f%f%f%f%f%f%f%f%f%f%f",sattime.read(),batvol,temp,ax,ay,az,gx,gy,gz,mx,my,mz);
+                    com.printf("ax:%f,ay:%f,az:%f\r\n",ax,ay,az);
+                    com.printf("gx:%f,gy:%f,gz:%f\r\n",gx,gy,gz);
+                    com.printf("mx:%f,my:%f,mz:%f\r\n",mx,my,mz);
+                    wait_ms(1000); 
                 }
             }
-            com.initialize();
+            com.initialize(); //initializing
         }
     }
+    sattime.stop();
+    sat.printf("From Sat : End of operation\r\n");
+    com.printf("From Sat : End of operation\r\n");
 }
