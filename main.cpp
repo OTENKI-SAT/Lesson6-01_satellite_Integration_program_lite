5ea6f2689782e7c31442992fec65087de2b3bc80
sonenagisa <93703736+sonenagisa@users.noreply.github.com>
2025-08-05 09:51:25 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include "mbed.h"
-#include "HEPTA_EPS.h"
-#include "HEPTA_CDH.h"
-#include "HEPTA_SENSOR.h"
-#include "HEPTA_COM.h"
-HEPTA_CDH cdh(PB_5, PB_4, PB_3, PA_8, "sd");
-HEPTA_EPS eps(PA_0,PA_4);
-HEPTA_SENSOR sensor(PA_7,PB_7,PB_6,0xD0);
-HEPTA_COM com(PA_9,PA_10,9600);
+#include "LITE_CDH.h"
+#include "LITE_EPS.h"
+#include "LITE_SENSOR.h"
+#include "LITE_COM.h"
+
+// センサまわりのインスタンス
+LITE_CDH    cdh(PB_5, PB_4, PB_3, PA_8, "sd", PA_3);
+LITE_EPS    eps(PA_0, PA_4);
+LITE_SENSOR sensor(PA_7, PB_7, PB_6);
+LITE_COM    com(PA_9,PA_10,9600); 
+RawSerial   sat(USBTX, USBRX, 9600); 
 DigitalOut condition(PB_1);
-RawSerial sat(USBTX,USBRX, 38400);
 Timer sattime;
+
+
 int main()
 {
     sat.printf("From Sat : Nominal Operation\r\n");
@@ -19,32 +23,18 @@ int main()
     int rcmd=0,cmdflag=0;  //command variable
     sattime.start();
     eps.turn_on_regulator();//turn on 3.3V conveter
-    sensor.setup();  
+   
     for(int i=0;i<50;i++){
         com.xbee_receive(&rcmd,&cmdflag);
-        
-        //satellite condition led
-        condition = !condition;
         
         //senssing HK data
         eps.vol(&batvol);
         sensor.temp_sense(&temp);
         
         //Transmitting HK data to Ground Station(GS)
-        com.printf("HEPTASAT::Condition = %d, Time = %f [s], batvol = %2f [V], temp = %2f [deg C]\r\n",flag,sattime.read(),batvol,temp);
-        wait_ms(1000);
+        com.printf("Hello\r\n");
         
-        //Power Saving Mode 
-        if((batvol <= 3.5)  | (temp > 35.0)){
-            eps.shut_down_regulator();
-            com.printf("Power saving mode ON\r\n"); 
-            flag = 1;
-        } else if((flag == 1) & (batvol > 3.7) & (temp <= 25.0)) {
-            eps.turn_on_regulator();
-            com.printf("Power saving mode OFF\r\n");
-            flag = 0;
-        }
-        
+         
         if(cmdflag == 1){
             if(rcmd == 'a'){
                 sat.printf("rcmd=%c,cmdflag=%d\r\n",rcmd,cmdflag);
@@ -52,8 +42,7 @@ int main()
                 sensor.temp_sense(&temp);
                 com.printf("Hepta-Sat Lite Uplink Ok\r\n");
                 for(int j=0;j<5;j++){
-                    com.printf("Time = %.f [s], voltage = [V], Temperature=%f[degC]\r\n",sattime.read(),batvol,temp);
-                    condition = 1;
+                    com.printf("Time = %f [s], batvol = %2f [V], temp = %2f [deg C]\r\n",sattime.read(),batvol,temp);
                     wait_ms(1000);
                 }
             }else if(rcmd == 'b') {
@@ -68,7 +57,6 @@ int main()
                 for(int i = 0; i < 10; i++) {
                     eps.vol(&batvol);
                     fprintf(fp,"%f\r\n",batvol);
-                    condition = 1;
                     wait_ms(1000);
                 }
                 fclose(fp);
@@ -98,4 +86,5 @@ int main()
     sattime.stop();
     sat.printf("From Sat : End of operation\r\n");
     com.printf("From Sat : End of operation\r\n");
+    
 }
